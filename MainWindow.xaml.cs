765c18bc1a9526cedb80e9e898f79eeb03eb6f9c
Alberto Castrejón Hernandez <betotenoch@gmail.com>
2023-07-07 08:06:40 -0500

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Empleados.Entities;
+using Empleados.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Empleados23AM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        EmpleadoServices services = new EmpleadoServices();
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Empleado empleado = new Empleado()
+            {
+                Name = txtNombre.Text,
+                LastName = txtApellido.Text,
+                Email = txtCorreo.Text,
+                RegisterDate = DateTime.Now
+            };
+            if (!string.IsNullOrEmpty(txtNombre.Text) || !string.IsNullOrEmpty(txtApellido.Text) || !string.IsNullOrEmpty(txtApellido.Text))
+            {
+                services.Add(empleado);
+                txtNombre.Clear();
+                txtApellido.Clear();
+                txtCorreo.Clear();
+                MessageBox.Show("Los datos han sido agregados correctamente");
+            }
+            else
+                MessageBox.Show("Los datos no sido agregados correctamente");
+        }
+        private void BtnRead_Click(object sender, RoutedEventArgs e)
+        {
+            int Id = int.Parse(txtId.Text);
+            Empleado empleado = services.Read(Id);
+            txtNombre.Text = empleado.Name;
+            txtApellido.Text = empleado.LastName;
+            txtCorreo.Text = empleado.Email;
+            txtFecha.Text = empleado.RegisterDate.ToString();
+            txtId.Text = empleado.PKEmpleado.ToString();
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int Id = int.Parse(txtId.Text);
+            if (string.IsNullOrEmpty(txtId.Text))
+            {
+                MessageBox.Show("ID NECESARIO");
+            }
+            else
+            {
+                services.Delete(Id);
+                txtNombre.Clear();
+                txtApellido.Clear();
+                txtCorreo.Clear();
+                txtFecha.Clear();
+                MessageBox.Show("Se elimino el usuario");
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            int Id = int.Parse(txtId.Text);//para obtener el valor
+            Empleado empleado = new Empleado();
+            empleado.PKEmpleado = Id;
+            empleado.Name = txtNombre.Text;
+            empleado.LastName = txtApellido.Text;
+            empleado.Email = txtCorreo.Text;
+            services.Update(empleado);
+            MessageBox.Show("Se modifico el registro");
+            //empleado.RegisterDate = DateTime.Now;            
+        }
+    }
+}
---
+++ Services/EmpleadoServices.cs
@@ -0,0 +1,103 @@
+﻿using Empleados.Context;
+using Empleados.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Empleados.Services
+{
+    public class EmpleadoServices
+    {
+        public void Add(Empleado request)
+        {
+            try
+            {   //abre la cadena de conexion y todo lo que se encuentre en using entrará a la DB
+                using (var _context = new ApplicationDbContext())//para usar la base de datos
+                {
+                    Empleado empleado = new Empleado()//recibe los
+                    {
+                        Name = request.Name,
+                        LastName = request.LastName,
+                        Email = request.Email,
+                        RegisterDate = DateTime.Now
+                    };
+                    _context.Empleados.Add(empleado);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Sucedió un error" + ex.Message);
+            }
+        }
+        public Empleado Read(int Id)
+        {
+            try
+            {
+
+                Empleado empleado = new Empleado();
+                using (var _context = new ApplicationDbContext())
+                {
+                    empleado = _context.Empleados.Find(Id);//Empleados.Where(varibale para consultar)
+                    return empleado;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Sucedió un error" + ex.Message);
+            }
+        }
+        public void Delete(int Id)
+        {
+            try
+            {
+                using (var _context = new ApplicationDbContext())
+                {
+                    Empleado empleado = _context.Empleados.Find(Id);
+                    if (empleado != null)
+                    {
+                        _context.Empleados.Remove(empleado);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        MessageBox.Show("NO HAY REGISTRO");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("ERROR: " + ex.Message);
+            }
+        }
+        public void Update(Empleado request)
+        {
+            try
+            {
+                using (var _context = new ApplicationDbContext())
+                {
+                    Empleado update = _context.Empleados.Find(request.PKEmpleado);
+                    update.Name = request.Name;
+                    update.LastName = request.LastName;
+                    update.Email = request.Email;
+                    update.RegisterDate = request.RegisterDate;
+                    _context.Empleados.Update(update);
+                    //_context.Entry(update).State = EntityState.Modified;
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Sucedio un error " + ex.Message);
+            }
+        }
+    }
+}
